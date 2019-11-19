36006940f57e8f2b5f30d6fef38b5e1d25f6d93e
bob <robin.lilius@gmail.com>
2019-11-19 11:19:31 +0100

    Trying fixed joints and midpoint, not working well

--- Assets/Scripts/DeformableSphere.cs
+++ Assets/Scripts/DeformableSphere.cs
@@ -10,11 +10,13 @@ public class DeformableSphere : MonoBehaviour
     public float maxThreshold = 0.99f;
     public float springDampening = 0.75f;
     public float springSpring = 10f;
+    public float midSpringDampening = 0.75f;
+    public float midSpringSpring = 10f;
     public float inputForce = 10;
     
     private Mesh mesh;
     private Vector3[] vertices;
-    private List<SpringJoint> jumpSprings;
+//    private List<SpringJoint> jumpSprings;
 
     private GameObject[] particles;
     
@@ -24,33 +26,40 @@ public class DeformableSphere : MonoBehaviour
         mesh = GetComponentInChildren<MeshFilter>().mesh;
         vertices = mesh.vertices;
         
-        particles = new GameObject[vertices.Length];
-        jumpSprings = new List<SpringJoint>();
+        particles = new GameObject[vertices.Length+1];
+//        jumpSprings = new List<SpringJoint>();
         
         Debug.Log("Mesh been loaded fo sho, "+vertices.Length+" vertices");
 
         for (int i = 0; i < vertices.Length; i++)
         {
-            var pos = transform.TransformPoint(vertices[i]);
-            particles[i] = Instantiate(particle, pos, Quaternion.identity);
+            var pos1 = transform.TransformPoint(vertices[i]);
+            particles[i] = Instantiate(particle, pos1, Quaternion.identity);
         }
 
-        for (int i = 0; i < particles.Length; i++)
+        var pos2 = gameObject.transform.position;
+        var midParticle = Instantiate(particle, pos2, Quaternion.identity);
+        particles[particles.Length - 1] = midParticle;
+
+        for (int i = 0; i < particles.Length-1; i++)
         {
             var p = particles[i];
-            for (int j = 0; j < particles.Length; j++)
+            for (int j = 0; j < particles.Length-1; j++)
             {
                 if (i == j) continue;
                 
                 var q = particles[j];
                 var dist = Vector3.Distance(p.transform.position, q.transform.position);
-                if (dist < minThreshold || dist > maxThreshold)
+                if (dist < minThreshold)
                 {
-                    var springJoint = AddSpring(p, q);
-                    if (dist > maxThreshold)
-                        jumpSprings.Add(springJoint);
+                    AddSpring(p, q, springDampening, springSpring);
+                    
+//                    var springJoint = AddSpring(p, q);
+//                    if (dist > maxThreshold)
+//                        jumpSprings.Add(springJoint);
                 }
             }
+            AddSpring(p, midParticle, midSpringDampening, midSpringSpring);
         }
         
         transform.position = Vector3.zero;
@@ -62,7 +71,7 @@ public class DeformableSphere : MonoBehaviour
     void Update()
     {
         
-        for (int i = 0; i < particles.Length; i++)
+        for (int i = 0; i < particles.Length-1; i++)
         {
             vertices[i] = particles[i].transform.position;
         }
@@ -86,28 +95,36 @@ public class DeformableSphere : MonoBehaviour
         if (Input.GetKey(KeyCode.W)) // UP
         {
             ApplyForceToParticles(Vector3.forward, inputForce);
+//            particles[particles.Length-1].transform.Rotate(10f, 0, 0, Space.World);
         }
         if (Input.GetKey(KeyCode.S)) // DOWN
         {
             ApplyForceToParticles(Vector3.back, inputForce);
         }
         
-        if (Input.GetKey(KeyCode.Space)) // JUMP
-        {
-            foreach (var s in jumpSprings)
-            {
-//                s.
-            }
-        }
+//        if (Input.GetKey(KeyCode.Space)) // JUMP
+//        {
+//            foreach (var s in jumpSprings)
+//            {
+////                s.
+//            }
+//        }
     }
 
-    private SpringJoint AddSpring(GameObject particle1, GameObject particle2)
+    private SpringJoint AddSpring(GameObject particle1, GameObject particle2, float dampening, float spring)
     {
         var springJoint = particle1.AddComponent<SpringJoint>();
         springJoint.connectedBody = particle2.GetComponent<Rigidbody>();
-        springJoint.damper = springDampening;
-        springJoint.spring = springSpring;
+        springJoint.damper = dampening;
+        springJoint.spring = spring;
         return springJoint;
+    }
+    
+    private FixedJoint AddFixed(GameObject particle1, GameObject particle2, float dampening, float spring)
+    {
+        var fixedJoint = particle1.AddComponent<FixedJoint>();
+        fixedJoint.connectedBody = particle2.GetComponent<Rigidbody>();
+        return fixedJoint;
     }
 
     private void ApplyForceToParticles(Vector3 direction, float force)
